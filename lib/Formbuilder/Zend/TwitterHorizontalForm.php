3f4126780ccac3933ebed85241f128fa8b0768ad
Stefan Hagspiel <shagspiel@dachcom.ch>
2016-11-17 12:27:57 +0100

    add download, html5File, imageTag to Bootstrap Wrapper

--- lib/Formbuilder/Zend/TwitterHorizontalForm.php
+++ lib/Formbuilder/Zend/TwitterHorizontalForm.php
@@ -14,4 +14,19 @@ class TwitterHorizontalForm extends \Twitter_Bootstrap3_Form_Horizontal {
         parent::__construct($formData);
     }
 
+    /**
+     * Retrieve a registered decorator for type element
+     *
+     * @param  string $type
+     * @return array
+     */
+    public function getDefaultDecoratorsByElementType($type)
+    {
+        if( in_array($type, array('download', 'html5File', 'imageTag') ) )
+        {
+            return parent::getDefaultDecoratorsByElementType('text');
+        }
+
+        return parent::getDefaultDecoratorsByElementType($type);
+    }
 }
--- lib/Formbuilder/Zend/TwitterVerticalForm.php
+++ lib/Formbuilder/Zend/TwitterVerticalForm.php
@@ -14,4 +14,19 @@ class TwitterVerticalForm extends \Twitter_Bootstrap3_Form_Vertical {
         parent::__construct($formData);
     }
 
+    /**
+     * Retrieve a registered decorator for type element
+     *
+     * @param  string $type
+     * @return array
+     */
+    public function getDefaultDecoratorsByElementType($type)
+    {
+        if( in_array($type, array('download', 'html5File', 'imageTag') ) )
+        {
+            return parent::getDefaultDecoratorsByElementType('text');
+        }
+
+        return parent::getDefaultDecoratorsByElementType($type);
+    }
 }
