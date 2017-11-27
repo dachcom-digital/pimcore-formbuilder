6c3d98e0de9a5a7fdd60dd901347596d63fbfff4
Stefan Hagspiel <shagspiel@dachcom.ch>
2017-11-27 09:55:44 +0100

    conditional logic #62: remove debug log

--- src/FormBuilderBundle/Resources/public/js/frontend/plugins/jquery.fb.ext.conditional-logic.js
+++ src/FormBuilderBundle/Resources/public/js/frontend/plugins/jquery.fb.ext.conditional-logic.js
@@ -405,7 +405,7 @@
                         return true;
                     }
                     var $conditionField = $dependencies.dependsOn(conditionSelector, actionOptions);
-                    console.log('add condition to', formDependingSelector, 'depends on: ', conditionSelector, 'actionOptions:', actionOptions);
+                    //console.log('add condition to', formDependingSelector, 'depends on: ', conditionSelector, 'actionOptions:', actionOptions);
 
                 });
 
