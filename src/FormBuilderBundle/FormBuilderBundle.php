20a1edbaebedeed91e0a9491aef79379f79e878e
Stefan Hagspiel <shagspiel@dachcom.ch>
2018-07-26 15:15:00 +0200

    bump version

--- src/FormBuilderBundle/FormBuilderBundle.php
+++ src/FormBuilderBundle/FormBuilderBundle.php
@@ -11,7 +11,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class FormBuilderBundle extends AbstractPimcoreBundle
 {
-    const BUNDLE_VERSION = '2.3.3';
+    const BUNDLE_VERSION = '2.3.4';
 
     /**
      * @param ContainerBuilder $container
