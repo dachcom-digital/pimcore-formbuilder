63ed286722c26e4a8b5b877c43949e263f04f67a
jpache <jpache@dachcom.ch>
2018-09-26 10:13:50 +0200

    Added PHPDoc to Storage Interfaces #115

--- src/FormBuilderBundle/Storage/FormFieldDynamicInterface.php
+++ src/FormBuilderBundle/Storage/FormFieldDynamicInterface.php
@@ -4,15 +4,33 @@ namespace FormBuilderBundle\Storage;
 
 interface FormFieldDynamicInterface
 {
+    /**
+     * @return string|false
+     */
     public function getName();
 
+    /**
+     * @return string
+     */
     public function getType();
 
+    /**
+     * @return string
+     */
     public function getOptions();
 
+    /**
+     * @return string
+     */
     public function getOptional();
 
+    /**
+     * @return int|mixed
+     */
     public function getOrder();
 
+    /**
+     * @return bool
+     */
     public function isUpdated();
 }
