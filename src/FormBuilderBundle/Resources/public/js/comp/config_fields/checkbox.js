9803d94bb20ceda8bf49f5bf6e7525f6d9ef5641
Manuel Hartung <mhartung@dachcom.ch>
2019-08-08 17:11:02 +0200

    enable html5 as checkbox for date-fieldtype, resolves #190

--- src/FormBuilderBundle/Resources/public/js/comp/config_fields/checkbox.js
+++ src/FormBuilderBundle/Resources/public/js/comp/config_fields/checkbox.js
@@ -1,11 +1,10 @@
 pimcore.registerNS('Formbuilder.comp.type.config_fields.checkbox');
 Formbuilder.comp.type.config_fields.checkbox = Class.create(Formbuilder.comp.type.config_fields.abstract, {
-    getField: function(fieldConfig, value)
-    {
+    getField: function (fieldConfig, value) {
         return new Ext.form.Checkbox({
             fieldLabel: fieldConfig.label,
             name: fieldConfig.id,
-            checked: false,
+            checked: fieldConfig.hasOwnProperty('config') && fieldConfig.config.hasOwnProperty('checked') ? fieldConfig.config.checked : false,
             uncheckedValue: false,
             inputValue: true,
             value: value
