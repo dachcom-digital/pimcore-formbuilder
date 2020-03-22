465cfaabd8301d9e9c2541f6894075897a3e4bfc
Stefan Hagspiel <shagspiel@dachcom.ch>
2020-03-22 22:29:46 +0100

    update migration

--- src/FormBuilderBundle/Migrations/Version20200304172137.php
+++ src/FormBuilderBundle/Migrations/Version20200304172137.php
@@ -13,8 +13,12 @@ class Version20200304172137 extends AbstractPimcoreMigration
     public function up(Schema $schema)
     {
         $this->addSql('ALTER TABLE formbuilder_forms ENGINE=InnoDB;');
-
-        // @todo: add migration data!!
+        $this->addSql('CREATE TABLE formbuilder_output_workflow_channel (id INT AUTO_INCREMENT NOT NULL, output_workflow INT DEFAULT NULL, type VARCHAR(190) NOT NULL, configuration LONGTEXT DEFAULT NULL COMMENT "(DC2Type:object)", INDEX IDX_CEC462362C75DDDC (output_workflow), PRIMARY KEY(id)) DEFAULT CHARACTER SET UTF8MB4 COLLATE `utf8mb4_general_ci` ENGINE = InnoDB;');
+        $this->addSql('CREATE TABLE formbuilder_output_workflow (id INT AUTO_INCREMENT NOT NULL, form_definition INT DEFAULT NULL, `name` VARCHAR(255) DEFAULT NULL, success_management LONGTEXT DEFAULT NULL COMMENT "(DC2Type:object)", INDEX IDX_BCB7909761F7634C (form_definition), UNIQUE INDEX name_form (name, form_definition), PRIMARY KEY(id)) DEFAULT CHARACTER SET UTF8MB4 COLLATE `utf8mb4_general_ci` ENGINE = InnoDB;');
+        $this->addSql('ALTER TABLE formbuilder_output_workflow_channel ADD CONSTRAINT FK_CEC462362C75DDDC FOREIGN KEY (output_workflow) REFERENCES formbuilder_output_workflow (id);');
+        $this->addSql('ALTER TABLE formbuilder_output_workflow ADD CONSTRAINT FK_BCB7909761F7634C FOREIGN KEY (form_definition) REFERENCES formbuilder_forms (id);');
+        $this->addSql('ALTER TABLE formbuilder_forms CHANGE mailLayout mailLayout LONGTEXT DEFAULT NULL COMMENT "(DC2Type:object)";');
+        $this->addSql('ALTER TABLE formbuilder_forms RENAME INDEX name TO UNIQ_29DA5346999517A;');
     }
 
     /**
--- src/FormBuilderBundle/Resources/install/sql/install.sql
+++ src/FormBuilderBundle/Resources/install/sql/install.sql
@@ -9,4 +9,25 @@ CREATE TABLE IF NOT EXISTS `formbuilder_forms` (
   `mailLayout` longtext COMMENT '(DC2Type:object)',
   PRIMARY KEY (`id`),
   UNIQUE KEY `UNIQ_29DA5346999517A` (`name`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `formbuilder_output_workflow` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `form_definition` int(11) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `success_management` longtext COMMENT '(DC2Type:object)',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name_form` (`name`,`form_definition`),
+  KEY `IDX_BCB7909761F7634C` (`form_definition`),
+  CONSTRAINT `FK_BCB7909761F7634C` FOREIGN KEY (`form_definition`) REFERENCES `formbuilder_forms` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `formbuilder_output_workflow_channel` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `output_workflow` int(11) DEFAULT NULL,
+  `type` varchar(190) NOT NULL,
+  `configuration` longtext COMMENT '(DC2Type:object)',
+  PRIMARY KEY (`id`),
+  KEY `IDX_CEC462362C75DDDC` (`output_workflow`),
+  CONSTRAINT `FK_CEC462362C75DDDC` FOREIGN KEY (`output_workflow`) REFERENCES `formbuilder_output_workflow` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
