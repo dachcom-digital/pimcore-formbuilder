a61464a4f5b6bd27b67e685732244a799f8e534a
Stefan Hagspiel <shagspiel@dachcom.ch>
2021-05-25 17:44:48 +0200

    allow better recaptcha error handling, originally #267 (#286)
    * allow better recaptcha error handling, originally #267

--- src/FormBuilderBundle/Resources/public/js/frontend/plugins/jquery.fb.ext.recaptcha-v3.js
+++ src/FormBuilderBundle/Resources/public/js/frontend/plugins/jquery.fb.ext.recaptcha-v3.js
@@ -63,7 +63,13 @@
         },
 
         injectTokenToForm: function () {
-            grecaptcha.execute(this.siteKey, {action: this.action}).then(this.onTokenGenerated.bind(this));
+            try {
+                grecaptcha.execute(this.siteKey, {action: this.action}).then(this.onTokenGenerated.bind(this), function () {
+                    this.$form.trigger('formbuilder.fatal-captcha', [null]);
+                }.bind(this));
+            } catch (error) {
+                this.$form.trigger('formbuilder.fatal-captcha', [error]);
+            }
         },
 
         /**
