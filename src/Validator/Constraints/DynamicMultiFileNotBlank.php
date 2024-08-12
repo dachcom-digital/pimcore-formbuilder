bc2d61117be873881427832a1de47c37046361a7
Stefan Hagspiel <info@solverat.com>
2024-08-12 14:59:51 +0200

    Allow custom message in DynamicMultiFileNotBlankValidator constraint

--- src/Validator/Constraints/DynamicMultiFileNotBlank.php
+++ src/Validator/Constraints/DynamicMultiFileNotBlank.php
@@ -7,4 +7,11 @@ use Symfony\Component\Validator\Constraint;
 class DynamicMultiFileNotBlank extends Constraint
 {
     public string $message = 'This value should not be blank.';
+
+    public function __construct(?array $options = null, ?string $message = null, ?array $groups = null, mixed $payload = null)
+    {
+        parent::__construct($options ?? [], $groups, $payload);
+
+        $this->message = $message ?? $this->message;
+    }
 }
